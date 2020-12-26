6e984478eba966283dcb62a8963cfd86cc642158
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2020-12-26 16:40:04 -0500

    set up basics in cdk and first lambda

---
+++ aws/deploy.sh
@@ -0,0 +1,1 @@
+cdk deploy --profile aram
---
+++ aws/lib/aws-stack.ts
@@ -0,0 +1,49 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3'
+import * as ssm from '@aws-cdk/aws-ssm'
+
+export class AwsStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const pinboardBucket = new s3.Bucket(this, 'PinboardLinks')
+    const storyBucket = new s3.Bucket(this, 'StoryLinks')
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('../lambdas'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+
+    const secretPinboardFeed = ssm.StringParameter.fromStringParameterAttributes(this, 'MySecureValue', {
+      parameterName: '/backreads/pinboardkey'
+    });    
+
+    const pinboardPull = new lambda.Function(this, 'PinboardPull', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('../lambdas/pinboard-pull'),  // code loaded from "lambda" directory
+      handler: 'pinboard-pull.handler', // file is "hello", function is "handler"
+      environment: {
+        DEPOSIT_BUCKET: pinboardBucket.bucketName,
+        FEED_NAME: secretPinboardFeed.stringValue
+      }
+    });
+    /**
+    const rssToJsonPull = new lambda.Function(this, 'RssToJsonPull', {
+      runtime: lambda.Runtime.PYTHON_3_7,    // execution environment
+      code: lambda.Code.fromAsset('../lambdas/rss-to-json'),  // code loaded from "lambda" directory
+      handler: 'rss-to-json.handler', // file is "hello", function is "handler"
+      environment: {
+        DEPOSIT_BUCKET: pinboardBucket.bucketName,
+        FEED_NAME: 'https://feeds.pinboard.in/rss/secret:7651932a7e7c6db975ea/u:AramZS/'
+      }
+    });
+     */
+
+    pinboardBucket.grantReadWrite(pinboardPull)
+    // pinboardBucket.grantReadWrite(rssToJsonPull)
+
+  }
+}

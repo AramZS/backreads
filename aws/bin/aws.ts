be72bfb1982ebea02ba87b1c88f19379f5e35c4e
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2020-12-27 17:51:16 -0500

    Set up static site hosting

--- aws/bin/aws.ts
+++ aws/bin/aws.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { AwsStack } from '../lib/aws-stack';
 
 const app = new cdk.App();
-new AwsStack(app, 'BackReads');
+new AwsStack(app, 'BackReads', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: 'us-east-1'
+    }
+});
--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -1,17 +1,141 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as s3 from '@aws-cdk/aws-s3'
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
 import * as ssm from '@aws-cdk/aws-ssm'
 import * as sfn from '@aws-cdk/aws-stepfunctions';
 import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { SfnStateMachine } from '@aws-cdk/aws-events-targets';
+import * as route53 from '@aws-cdk/aws-route53'
+import targets = require('@aws-cdk/aws-route53-targets/lib');
+import * as route53Patterns from '@aws-cdk/aws-route53-patterns';
+import * as cloudfront from '@aws-cdk/aws-cloudfront'
+import { DnsValidatedCertificate, ValidationMethod, CertificateValidation } from "@aws-cdk/aws-certificatemanager";
+import { SPADeploy } from './cdk-spa-deploy'
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Adapted from https://bahr.dev/2020/09/01/multiple-frontends/
+    const domain = 'backreads.com'
+
+    const hostedZone = route53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName: domain
+    })
+
+    new cdk.CfnOutput(this, 'Site', {value: 'https://'+domain})
+
     // The code that defines your stack goes here
     const pinboardBucket = new s3.Bucket(this, 'PinboardLinks')
     const storyBucket = new s3.Bucket(this, 'StoryLinks')
+    const backreadsSiteBucket = new s3.Bucket(this, 'BackreadsSite', {
+      bucketName: domain,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html'
+    })
+
+    new cdk.CfnOutput(this, 'Bucket', { value: backreadsSiteBucket.bucketName })
+
+
+    const certificate = new DnsValidatedCertificate(this, "SiteCertificate", {
+      region: 'us-east-1',
+      hostedZone: hostedZone,
+      domainName: domain,
+      subjectAlternativeNames: [`*.${domain}`],
+      // validation: CertificateValidation.fromDns(),
+      validationDomains: {
+        [domain]: domain,
+        [`*.${domain}`]: domain
+      },
+      validationMethod: ValidationMethod.DNS,
+    })
+
+    new cdk.CfnOutput(this, 'Certificate', 
+      { value: certificate.certificateArn }
+    );
+
+
+
+    /**
+    // CloudFront distribution that provides HTTPS
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+      aliasConfiguration: {
+          acmCertRef: certificate.certificateArn,
+          names: [ domain ],
+          sslMethod: cloudfront.SSLMethod.SNI,
+          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+      },
+      originConfigs: [
+          {
+              customOriginSource: {
+                  domainName: backreadsSiteBucket.bucketWebsiteDomainName,
+                  originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+              },          
+              behaviors : [ {isDefaultBehavior: true}],
+          }
+      ]
+    });
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });    
+
+    // Route53 alias record for the CloudFront distribution
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: domain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      zone: hostedZone
+    });
+
+    new HttpsRedirect(this, 'Redirect', {
+      zone: hostedZone,
+      recordNames: [`www.${domain}`],
+      targetDomain: domain
+    })
+     */
+
+    const deployment = new SPADeploy(this, 'spaDeployment').createSiteWithCloudfront({
+      indexDoc: 'index.html',
+      websiteFolder: '../static',
+      certificateARN: certificate.certificateArn,
+      cfAliases: [domain, `*.${domain}`]
+
+    })
+    const cloudfrontTarget = route53.RecordTarget
+        .fromAlias(new targets.CloudFrontTarget(deployment.distribution));
+    /**
+    const siteDeployment = new s3Deployment.BucketDeployment(
+      this,
+      'deployStaticWebsite',
+      {
+          sources: [s3Deployment.Source.asset('../static')],
+          destinationBucket: backreadsSiteBucket,
+          distribution,
+          distributionPaths: ['/*']
+      }
+    );
+    */
+
+    // Route53 alias record for the CloudFront distribution
+    new route53.ARecord(this, 'ARecord', {
+      recordName: domain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(deployment.distribution)),
+      zone: hostedZone
+    });
+
+    new route53.ARecord(this, 'WildCardARecord', {
+      recordName: `*.${domain}`,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(deployment.distribution)),
+      zone: hostedZone
+    });
+
+    new route53Patterns.HttpsRedirect(this, 'redirectWWW', {
+      zone: hostedZone,
+      recordNames: [`www.${domain}`],
+      targetDomain: domain
+    })
+
     // defines an AWS Lambda resource
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
@@ -86,10 +210,19 @@ export class AwsStack extends cdk.Stack {
           .when(sfn.Condition.numberEquals('$.StatusCode', 200), jobResolution));
           // .otherwise(waitX));
 
-    new sfn.StateMachine(this, 'StateMachine', {
+    const linkBuilderStateMachine = new sfn.StateMachine(this, 'StateMachine', {
       definition,
       timeout: cdk.Duration.minutes(5)
     });
+
+    // Don't forget the scheduled time is UTC
+    const dailyLinkPull = new Rule(this, 'ScheduleRule', {
+      // 4am et
+      schedule: Schedule.cron({ minute: '00', hour: '09', }),
+      targets: [new SfnStateMachine(linkBuilderStateMachine)]
+     });
+
+     // dailyLinkPull.addTarget()
 
   }
 }

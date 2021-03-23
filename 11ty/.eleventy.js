9087b4cae27c122cda75a566e39ee00fd2856327
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2021-03-22 21:08:52 -0400

    Trying to fix up eleventy implimentation

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -1,6 +1,17 @@
-module.exports = {
-	dir: {
-	  input: "./",
-	  output: "../static"
+module.exports = function(eleventyConfig) {
+	// Universal filters add to:
+	// * Liquid
+	// * Nunjucks
+	// * Handlebars
+	// * JavaScript (New in 0.7.0)
+	eleventyConfig.addFilter("myFilter", function(value) {
+	  return value;
+	});
+
+	return { 
+		dir: {
+			input: "./",
+			output: "../static"
+		}
 	}
   };

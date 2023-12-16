36b348798138ddf48b8c820650d00f4f579757ac
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2023-12-15 22:46:51 -0500

    Add a note to the lambda test

--- lambdas/pinboard-pull/test.js
+++ lambdas/pinboard-pull/test.js
@@ -1,19 +1,23 @@
-var test = async function() {
-	var f = require('node-fetch'); 
-	var x = require('xml2js'); 
-	console.log('start')
-	var response = await f('https://feeds.pinboard.in/rss/secret:7651932a7e7c6db975ea/u:AramZS/');
+var test = async function () {
+	var f = require("node-fetch");
+	var x = require("xml2js");
+	console.log("start");
+	var response = await f(
+		"https://feeds.pinboard.in/rss/secret:7651932a7e7c6db975ea/u:AramZS/"
+	);
 	responseText = await response.text();
-	console.log('output', responseText)
+	console.log("output", responseText);
 	var rss = await x.parseStringPromise(responseText);
-	console.log('output js', rss)
-	console.log('output js item', rss['rdf:RDF'].item)
-	return rss['rdf:RDF'].item.map((item) => {
-		if (item && item['$'] && item['$']['rdf:about']){
-			return item['$']['rdf:about'];
+	console.log("output js", rss);
+	console.log("output js item", rss["rdf:RDF"].item);
+	return rss["rdf:RDF"].item.map((item) => {
+		if (item && item["$"] && item["$"]["rdf:about"]) {
+			return item["$"]["rdf:about"];
 		}
 	});
-}
+};
 var testOutput = test();
 
-setTimeout(() => console.log('done', testOutput), 10000)
+setTimeout(() => console.log("done", testOutput), 10000);
+
+// Run via `node ./lambdas/pinboard-pull/test.js`
